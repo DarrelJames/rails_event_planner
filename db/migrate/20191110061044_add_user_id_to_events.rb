3507ed84b92546798c2a3bd5cfcbe84d381ff947
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-09 20:44:38 -0800

    fixed migration new schema

--- db/migrate/20191110061044_add_user_id_to_events.rb
+++ db/migrate/20191110061044_add_user_id_to_events.rb
@@ -1,5 +1,5 @@
 class AddUserIdToEvents < ActiveRecord::Migration[6.0]
   def change
-    add_reference :events, :user, null: false, foreign_key: true
+    add_reference :events, :user
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_10_060240) do
+ActiveRecord::Schema.define(version: 2019_11_10_061044) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_11_10_060240) do
     t.integer "venue_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
     t.index ["venue_id"], name: "index_events_on_venue_id"
   end
 
