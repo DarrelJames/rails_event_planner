f671e5688cb3e04fac9d192c66963b9a813d2bb5
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-09 20:44:38 -0800

    change to datetime

--- db/migrate/20191110055059_create_events.rb
+++ db/migrate/20191110055059_create_events.rb
@@ -2,9 +2,9 @@ class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
       t.string :name
-      t.string :date
-      t.string :start_time
-      t.string :end_time
+      t.datetime :date
+      t.datetime :start_time
+      t.datetime :end_time
       t.references :venue, null: false, foreign_key: true
 
       t.timestamps
