01bbc0a619d8252c015e597174fa197bfb0d5845
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-10 02:29:15 -0800

    Set event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,24 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
   def new
+    @event = Event.new
   end
 
   def show
   end
 
   def index
+    @events = current_user.events
+  end
+
+  private
+
+  def set_event
+    @event = Event.find_by_id(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:name)
   end
 end
