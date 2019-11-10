b03fb4299becab6723a2f67af0d62740810ea760
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-10 03:20:40 -0800

    Whitelist attributes. Add logic for new and create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,8 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def new
-    @event = Event.new
+    @event = Event.new(start_time: Time.now, end_time: Time.now)
+    @venue = @event.build_venue
   end
 
   def show
@@ -12,6 +13,12 @@ class EventsController < ApplicationController
     @events = current_user.events
   end
 
+  def create
+    @event = Event.create(event_params)
+
+    redirect_to event_path(@event)
+  end
+
   private
 
   def set_event
@@ -19,6 +26,6 @@ class EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:name)
+    params.require(:event).permit(:name, :date, :start_time, :end_time)
   end
 end
