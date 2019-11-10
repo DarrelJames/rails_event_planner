f360c7ba71ae376183be46ef0031a5f50dd62a62
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-10 11:08:02 -0800

    Guests can see events

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new
-    if user.admin?
+    can :read, Event
+
+    if user.present?
       can :manage, Event, user_id: user.id
-    else
-      can :read, :all
+      
     end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
