078bbc187032b7165444e2ed4f3571b0ea29f779
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-10 11:34:54 -0800

    removed generated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'events/new'
-  get 'events/show'
-  get 'events/index'
   devise_for :users
-
   root 'static#index'
   resources :guests
   resources :events
