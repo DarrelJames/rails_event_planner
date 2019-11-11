9a3ba477249513aec2b2f8073ca09c4a78a9fd5d
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-10 20:49:35 -0800

    set guest and params

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,10 +1,27 @@
 class GuestsController < ApplicationController
+  before_action :set_guest, only: [:show, :edit, :update, :destroy]
+
   def new
+    @guest = Guest.new
   end
 
   def edit
   end
 
   def show
+  end
+
+  def index
+    @guests = Guest.all
+  end
+
+  private
+
+  def set_guest
+    @guest = Guest.find_by_id(params[:idea])
+  end
+
+  def guest_params
+    params.require(:guest).permit(:first_name, :last_name, :street, :state, :city, :zip_code)
   end
 end
