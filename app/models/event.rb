408eb6087fb034a3a9eb4e8a1241ea1883ec5ff4
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-11 20:49:09 -0800

    Validate name date time

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,8 @@ class Event < ApplicationRecord
   has_many :guests, through: :rsvps
   belongs_to :venue
 
+  validates :name, :date, :start_time, :end_time, presence: true
+
   # accepts_nested_attributes_for :venue
 
   def venue_attributes=(venue_attributes)
