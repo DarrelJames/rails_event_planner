d8a15f8bb9d1d86d37aa7c0f6bb085f5fb902a9f
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-11 23:33:02 -0800

    Can rsvp. updates responses

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,7 +1,35 @@
 class RsvpsController < ApplicationController
-  def show
+
+  def index
+    @rsvp = Rsvp.find_by(guest_id: session[:guest_id], event_id: params[:event_id])
   end
 
-  def new
+  def update
+    @rsvp = Rsvp.find_by(guest_id: session[:guest_id], event_id: params[:event_id])
+    @rsvp.update(rsvp_params)
+    redirect_to event_rsvps_path
+  end
+
+  def create
+    @event = Event.find_by_id(params[:event_id])
+    @guest = @event.guests.find_by(first_name: params[:first_name], last_name: params[:last_name])
+
+    if @guest
+
+      session[:guest_id] = @guest.id
+      @rsvp = Rsvp.find_by(guest_id: @guest.id, event_id: @event.id)
+
+      redirect_to event_rsvps_path
+    else
+      flash[:error] = "Sorry, couldn't find your name on the guestlist."
+
+      redirect_to event_rsvps_path
+    end
+  end
+
+  private
+
+  def rsvp_params
+    params.require(:rsvp).permit(:response)
   end
 end
