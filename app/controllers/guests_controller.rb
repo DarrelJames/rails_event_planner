23cec680889a95e308fb9837b89f52026e503603
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-11 17:39:48 -0800

    create and update guest actions

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -9,19 +9,38 @@ class GuestsController < ApplicationController
   end
 
   def show
+
   end
 
   def index
     @guests = Guest.all
   end
 
+  def create
+    @guest = current_user.guests.build(guest_params)
+    binding.pry
+    if @guest.save
+      redirect_to guests_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @guest.update(guest_params)
+      redirect_to @guest
+    else
+      render :edit
+    end
+
+  end
   private
 
   def set_guest
-    @guest = Guest.find_by_id(params[:idea])
+    @guest = Guest.find_by_id(params[:id])
   end
 
   def guest_params
-    params.require(:guest).permit(:first_name, :last_name, :street, :state, :city, :zip_code)
+    params.require(:guest).permit(:first_name, :last_name, :street, :state, :city, :zip_code, :event_ids => [])
   end
 end
