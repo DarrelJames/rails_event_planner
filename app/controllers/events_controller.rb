47fae5e7cb7342f5a1bb6758e61049fd055e15b0
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-13 20:45:08 -0800

    Redirect to events_path

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,7 +20,7 @@ class EventsController < ApplicationController
     @event = current_user.events.build(event_params)
 
     if @event.save
-      redirect_to event_path(@event)
+      redirect_to events_path
     else
       render :new
     end
