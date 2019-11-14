7bb8c4bf20c0e6dbcda709cdacf78e182771ea47
Darrel Castellano <dcastellano2007@gmail.com>
2019-11-13 19:21:49 -0800

    Find name case insensitive

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -9,6 +9,10 @@ class Guest < ApplicationRecord
     "#{self.first_name} #{self.last_name}"
   end
 
+  def self.full_name_nocase(first_name, last_name)
+    where("LOWER(first_name) = ? AND LOWER(last_name) = ?", first_name.downcase, last_name.downcase).first
+  end
+
   def self.by_event(event)
     joins(:rsvps).where(rsvps: {event_id: event}).distinct
   end
